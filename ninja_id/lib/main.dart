b4bf75f0c093cc00adc84c36b00e86c777437bf5
manas-anarov <you@example.com>
2020-07-17 15:29:03 +0600

    20

--- ninja_id/lib/main.dart
+++ ninja_id/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card.dart';
+
 void main() => runApp(MaterialApp(
   home:QuoteList(),
 ));
@@ -17,32 +19,9 @@ class _QuoteListState extends State<QuoteList> {
    Quote(author: 'nug', text: 'asdasdas'),
   ];
 
-  Widget quoteTemplate(quote){
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              quote.text, 
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[600] ,
-              ),),
-              SizedBox(height: 6.0,),
-              Text(
-                quote.author,
-                style: TextStyle(
-                  fontSize: 14.0,
-                  color: Colors.grey[600],
-                )
-              )
-          ],),
-      ), 
-    );
-  }
+  // Widget quoteTemplate(quote){
+  //   return  QuoteCard(quote: quote);
+  // }
 
 
   @override
@@ -56,7 +35,7 @@ class _QuoteListState extends State<QuoteList> {
           backgroundColor: Colors.redAccent,
         ),
         body: Column(
-          children: quotes.map((quote) => quoteTemplate(quote)).toList(),
+          children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
         ),
         );
 
